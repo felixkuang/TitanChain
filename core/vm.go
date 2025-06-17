d8a9d3afe976ff40a7e8327aae168468691a2bca
felixkuang <562106689@qq.com>
2025-06-17 11:51:57 +0800

    重构虚拟机、支持字符串

--- core/vm.go
+++ core/vm.go
@@ -8,14 +8,52 @@ const (
 	InstrPush Instruction = 0x0a // 10
 	// InstrAdd 表示对栈顶两个元素进行加法操作的指令。
 	InstrAdd Instruction = 0x0b // 11
+	// InstrPushByte 表示将单字节数据压入栈的指令。
+	InstrPushByte Instruction = 0x0c
+	// InstrPack 表示将多个字节打包为字节切片的指令。
+	InstrPack Instruction = 0x0d
+	// InstrSub 表示对栈顶两个元素进行减法操作的指令。
+	InstrSub Instruction = 0x0e // 14
 )
+
+// Stack 表示虚拟机的操作数栈，支持任意类型元素的入栈和出栈操作。
+type Stack struct {
+	data []any // 存储栈中元素的切片
+	sp   int   // 栈顶指针，指向下一个可用位置
+}
+
+// NewStack 创建一个指定大小的新栈。
+// size: 栈的最大容量。
+// 返回 *Stack：新建的栈实例。
+func NewStack(size int) *Stack {
+	return &Stack{
+		data: make([]any, size),
+		sp:   0,
+	}
+}
+
+// Push 将元素 v 压入栈顶。
+// v: 任意类型的待入栈元素。
+func (s *Stack) Push(v any) {
+	s.data[s.sp] = v
+	s.sp++
+}
+
+// Pop 弹出栈顶元素并返回。
+// 返回 any：被弹出的元素。
+func (s *Stack) Pop() any {
+	value := s.data[0]
+	s.data = append(s.data[:0], s.data[1:]...)
+	s.sp--
+
+	return value
+}
 
 // VM 表示一个简单的字节码虚拟机，用于执行智能合约或脚本。
 type VM struct {
 	data  []byte // 待执行的字节码数据
 	ip    int    // 指令指针，指向当前执行的指令位置
-	stack []byte // 操作数栈
-	sp    int    // 栈指针，指向当前栈顶
+	stack *Stack
 }
 
 // NewVM 创建一个新的虚拟机实例，data 为待执行的字节码数据。
@@ -23,8 +61,7 @@ func NewVM(data []byte) *VM {
 	return &VM{
 		data:  data,
 		ip:    0,
-		stack: make([]byte, 1024),
-		sp:    -1,
+		stack: NewStack(128),
 	}
 }
 
@@ -54,22 +91,33 @@ func (vm *VM) Run() error {
 func (vm *VM) Exec(instr Instruction) error {
 	switch instr {
 	case InstrPush:
-		// InstrPush: 将前一个字节压入栈
-		vm.pushStack(vm.data[vm.ip-1])
+		vm.stack.Push(int(vm.data[vm.ip-1]))
+
+	case InstrPushByte:
+		vm.stack.Push(byte(vm.data[vm.ip-1]))
+
+	case InstrPack:
+		n := vm.stack.Pop().(int)
+		b := make([]byte, n)
+
+		for i := 0; i < n; i++ {
+			b[i] = vm.stack.Pop().(byte)
+		}
+
+		vm.stack.Push(b)
+
+	case InstrSub:
+		a := vm.stack.Pop().(int)
+		b := vm.stack.Pop().(int)
+		c := a - b
+		vm.stack.Push(c)
+
 	case InstrAdd:
-		// InstrAdd: 取出栈顶两个元素相加，并将结果压入栈
-		a := vm.stack[0]
-		b := vm.stack[1]
+		a := vm.stack.Pop().(int)
+		b := vm.stack.Pop().(int)
 		c := a + b
-		vm.pushStack(c)
+		vm.stack.Push(c)
 	}
 
 	return nil
-}
-
-// pushStack 将一个字节压入操作数栈。
-// b: 需要压入栈的字节。
-func (vm *VM) pushStack(b byte) {
-	vm.sp++
-	vm.stack[vm.sp] = b
 }
--- core/vm_test.go
+++ core/vm_test.go
@@ -6,6 +6,19 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+func TestStack(t *testing.T) {
+	s := NewStack(128)
+
+	s.Push(1)
+	s.Push(2)
+
+	value := s.Pop()
+	assert.Equal(t, value, 1)
+
+	value = s.Pop()
+	assert.Equal(t, value, 2)
+}
+
 // TestVM 测试简单虚拟机的基本指令执行流程。
 // 用例说明：
 // - 构造字节码序列，依次将两个数（2、2）压入栈，再执行加法指令，期望栈顶为 4。
@@ -20,9 +33,14 @@ func TestVM(t *testing.T) {
 	// 3
 	// push stack
 
-	data := []byte{0x02, 0x0a, 0x02, 0x0a, 0x0b}
+	data := []byte{0x03, 0x0a, 0x02, 0x0a, 0x0e}
+	// data := []byte{0x03, 0x0a, 0x46, 0x0c, 0x4f, 0x0c, 0x4f, 0x0c, 0x0d}
 	vm := NewVM(data)
 	assert.Nil(t, vm.Run())
 
-	assert.Equal(t, byte(4), vm.stack[vm.sp])
+	result := vm.stack.Pop().(int)
+
+	assert.Equal(t, 1, result)
+
+	// assert.Equal(t, "FOO", string(result))
 }
